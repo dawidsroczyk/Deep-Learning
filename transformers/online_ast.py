68070c2ee9d009af112b29d4c25cf01c00e41748
dawidsroczyk <dawid.sroczyk54@gmail.com>
2025-04-20 13:07:58 +0000

    bugfix

--- transformers/online_ast.py
+++ transformers/online_ast.py
@@ -95,6 +95,7 @@ def train_ast(num_epochs, num_classes, lr, weight_decay, best_model_path, data_p
         total_loss = 0
         for idx, (seq, labels) in enumerate(train_dataloader):
             print(f'{idx} / {len(train_dataloader)}')
+            seq, labels = seq.to(device), labels.to(device)
             optimizer.zero_grad()
             logits = model(seq)
             loss = criterion(logits, labels)
