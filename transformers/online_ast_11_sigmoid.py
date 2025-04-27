a4c1d470ba9c6b2d58b06bb3319edb51a833fc8c
dawidsroczyk <dawid.sroczyk54@gmail.com>
2025-04-27 15:29:36 +0000

    changes

--- transformers/online_ast_11_sigmoid.py
+++ transformers/online_ast_11_sigmoid.py
@@ -223,7 +223,7 @@ def train_ast(num_epochs, num_classes, lr, weight_decay, model_path,
         total_loss = 0
 
         for idx, (seq, labels) in enumerate(train_dataloader):
-            if idx % 1000 == 0:
+            if idx % 100 == 0:
                 print(f'{idx} / {len(train_dataloader)}')
             seq, labels = seq.to(device), labels.to(device)
             one_hot_labels = F.one_hot(labels, num_classes=num_classes).float().squeeze(1)
