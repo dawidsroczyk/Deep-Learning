3343afde70a7a5f92512b1dccbad9c30fa82a7d3
dsroczyk <dsroczyk@griddynamics.com>
2025-05-09 15:02:57 +0200

    changes

--- generative_models/variational_autoencoder.py
+++ generative_models/variational_autoencoder.py
@@ -1,112 +1,135 @@
 import torch
 import torch.nn as nn
-import matplotlib.pyplot as plt
-import os
-from PIL import Image
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms
-import data_manager as dm
 
 class VariationalEncoder(nn.Module):
-    def __init__(self, img_size=64, emb_dimension=2, device='cpu', *args, **kwargs):
+    def __init__(self, img_size=64, emb_dimension=2, device='cpu', 
+                 in_channels=3, base_channels=128, num_blocks=4, 
+                 kernel_size=2, stride=2, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.device = device
-
-        def create_block(in_channels, out_channels, kernel_size=2, stride=2):
+        self.emb_dimension = emb_dimension
+        
+        def create_block(in_channels, out_channels, kernel_size, stride):
             return nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size, stride),
                 nn.BatchNorm2d(out_channels),
                 nn.LeakyReLU()
             )
-        self.emb_dimension = emb_dimension
         
-        in_channels = 3
-        out_channels = 128
-        kernel_size = 2
-        stride = 2
-
-        self.block_1 = create_block(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
-        self.block_2 = create_block(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
-        self.block_3 = create_block(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
-        self.block_4 = create_block(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
+        # Create encoder blocks
+        self.blocks = nn.ModuleList()
+        current_channels = in_channels
+        for i in range(num_blocks):
+            self.blocks.append(
+                create_block(current_channels, base_channels, kernel_size, stride)
+            )
+            current_channels = base_channels
+        
         self.flatten = nn.Flatten()
-        in_dim = img_size * img_size // 256 * out_channels
+        
+        # Calculate the linear layer input dimension
+        downsampled_size = img_size // (stride ** num_blocks)
+        in_dim = (downsampled_size ** 2) * base_channels
+        
         self.mean = nn.Linear(in_dim, emb_dimension)
         self.log_var = nn.Linear(in_dim, emb_dimension)
     
     def forward(self, x):
-        x = self.block_1(x)
-        x = self.block_2(x)
-        x = self.block_3(x)
-        x = self.block_4(x)
+        for block in self.blocks:
+            x = block(x)
+        
         x = self.flatten(x)
-
+        
         mean = self.mean(x)
         log_var = self.log_var(x)
         
         eps = torch.randn(size=(x.shape[0], self.emb_dimension), device=self.device)
         x = mean + torch.exp(0.5 * log_var) * eps
-
+        
         return x
 
 class Decoder(nn.Module):
-    def __init__(self, emb_dimension=2, img_size=64, device='cpu', *args, **kwargs):
+    def __init__(self, emb_dimension=2, img_size=64, device='cpu',
+                 out_channels=3, base_channels=128, num_blocks=4,
+                 kernel_size=2, stride=2, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.img_size = img_size
-        self.out_channels = 128
         self.device = device
-
-        out_channels = 128
-        in_channels = 3
-        kernel_size = 2
-        stride = 2
-
-        def create_block(in_channels, out_channels):
+        self.base_channels = base_channels
+        
+        def create_block(in_channels, out_channels, kernel_size, stride):
             return nn.Sequential(
-                nn.ConvTranspose2d(in_channels, in_channels, kernel_size, stride),
-                nn.BatchNorm2d(in_channels),
+                nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride),
+                nn.BatchNorm2d(out_channels),
                 nn.LeakyReLU()
             )
-
-        in_dim = img_size * img_size // 256 * out_channels
-
+        
+        # Calculate the linear layer output dimension
+        downsampled_size = img_size // (stride ** num_blocks)
+        in_dim = (downsampled_size ** 2) * base_channels
+        
         self.lin_1 = nn.Linear(emb_dimension, in_dim)
         self.batch_norm_1 = nn.BatchNorm1d(in_dim)
         self.leaky_relu_1 = nn.LeakyReLU()
-
-        self.block_1 = create_block(out_channels, out_channels)
-        self.block_2 = create_block(out_channels, out_channels)
-        self.block_3 = create_block(out_channels, out_channels)
-        self.block_4 = create_block(out_channels, out_channels)
-
-        self.final_conv2d = nn.ConvTranspose2d(out_channels, in_channels, 1, 1)
+        
+        # Create decoder blocks
+        self.blocks = nn.ModuleList()
+        for i in range(num_blocks):
+            self.blocks.append(
+                create_block(base_channels, base_channels, kernel_size, stride)
+            )
+        
+        self.final_conv2d = nn.ConvTranspose2d(base_channels, out_channels, 1, 1)
     
     def forward(self, x):
-
         x = self.lin_1(x)
         x = self.batch_norm_1(x)
         x = self.leaky_relu_1(x)
-
-        x = torch.reshape(x, (-1, self.out_channels, self.img_size // 16, self.img_size // 16))
-
-        x = self.block_1(x)
-        x = self.block_2(x)
-        x = self.block_3(x)
-        x = self.block_4(x)
-
+        
+        # Reshape to start the convolutional transpose path
+        downsampled_size = self.img_size // (2 ** len(self.blocks))
+        x = torch.reshape(x, (-1, self.base_channels, downsampled_size, downsampled_size))
+        
+        for block in self.blocks:
+            x = block(x)
+        
         x = self.final_conv2d(x)
-
         return x
 
 class VariationalAutoencoder(nn.Module):
-    def __init__(self, img_size, emb_dimension, device, *args, **kwargs):
+    def __init__(self, img_size=64, emb_dimension=2, device='cpu', 
+                 in_channels=3, base_channels=128, num_blocks=4,
+                 kernel_size=2, stride=2, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.img_size = img_size
         self.emb_dimension = emb_dimension
         self.device = device
         
-        self.encoder = VariationalEncoder(img_size, emb_dimension, device)
-        self.decoder = Decoder(emb_dimension, img_size, device)
+        # Initialize encoder and decoder with matching architecture
+        encoder_params = {
+            'img_size': img_size,
+            'emb_dimension': emb_dimension,
+            'device': device,
+            'in_channels': in_channels,
+            'base_channels': base_channels,
+            'num_blocks': num_blocks,
+            'kernel_size': kernel_size,
+            'stride': stride
+        }
+        
+        decoder_params = {
+            'emb_dimension': emb_dimension,
+            'img_size': img_size,
+            'device': device,
+            'out_channels': in_channels,
+            'base_channels': base_channels,
+            'num_blocks': num_blocks,
+            'kernel_size': kernel_size,
+            'stride': stride
+        }
+        
+        self.encoder = VariationalEncoder(**encoder_params)
+        self.decoder = Decoder(**decoder_params)
     
     def forward(self, x):
         x = self.encoder(x)
