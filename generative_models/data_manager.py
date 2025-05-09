45b8fa19590697dc8dba45e48c5356f00817baf6
dsroczyk <dsroczyk@griddynamics.com>
2025-05-09 11:41:42 +0200

    device

--- generative_models/data_manager.py
+++ generative_models/data_manager.py
@@ -1,5 +1,11 @@
 import os
+import kagglehub
 
-def full_dataset_path():
+def full_dataset_path_local():
     path = '/home/dawid/repos/Deep-Learning/generative_models/data/cats/Data/'
+    return path
+
+def full_dataset_path_kaggle():
+    path = kagglehub.dataset_download("borhanitrash/cat-dataset")
+    path = os.path.join(path, 'cats', 'Data')
     return path
